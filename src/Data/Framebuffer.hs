4f4151799b3c100bcbd56a43951b26d71297741f
0xd34df00d <0xd34df00d@gmail.com>
2019-02-18 17:41:01 -0500

    Use Data.Array for storing the pixels

--- src/Data/Framebuffer.hs
+++ src/Data/Framebuffer.hs
@@ -1,9 +1,18 @@
+{-# LANGUAGE RecordWildCards, OverloadedStrings #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
 module Data.Framebuffer where
+
+import qualified Data.Array.IArray as A
 
 newtype Pixel = Pixel (Double, Double, Double) deriving (Eq, Ord, Show)
 
+newtype Width = Width { getWidth :: Int } deriving (Eq, Ord, Show, Enum, Num, Real, Integral, A.Ix)
+newtype Height = Height { getHeight :: Int } deriving (Eq, Ord, Show, Enum, Num, Real, Integral, A.Ix)
+newtype Idx = Idx (Width, Height) deriving (Eq, Ord, Show, A.Ix)
+
 data Framebuffer = Framebuffer
-  { width :: Int
-  , height :: Int
-  , pixels :: [Pixel]
+  { width :: Width
+  , height :: Height
+  , pixels :: A.Array Idx Pixel
   } deriving (Eq, Ord, Show)
